3ddb0655e609b1244c086e040337340fbdfc2b3c
Luiz Casula <luizcasulaf@gmail.com>
2020-05-11 12:49:47 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+var cbLocationElement = document.getElementById('cbLocation');
+
 axios.get('https://apidosetoreletrico.com.br/api/energy-providers')
     .then(function (response) {
-        console.log(response);
+        // console.log(response.data);
+        var locations = response.data;
+        locations.forEach(element => {
+            var locationItem = document.createElement('option');
+            locationItem.setAttribute('id', '' + element.id + '');
+            locationItem.setAttribute('onselect', 'printId(' + element.id + ')');
+            var locationText = document.createTextNode(element.name);
+            locationItem.appendChild(locationText);
+            cbLocationElement.appendChild(locationItem);
+        });
     })
     .catch(function (error) {
         console.warn(error);
     });
+
+function printId(id) {
+    console.log(id);
+}
+
+
+
+
