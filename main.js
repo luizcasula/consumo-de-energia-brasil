4181af9d8b4e425e58bdac70aedf0af139259ed6
Luiz Casula <luizcasulaf@gmail.com>
2020-05-28 23:49:49 -0400

    remove and save store

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // const data = require('./public/data.json');
 
-// console.log(data);
-
 const cbPowerSupply = document.getElementById('cbPowerSupply');
 const inputPowerEletric = document.getElementById('inputPowerEletric');
 const inputAmountDevices = document.getElementById('inputAmountDevices');
@@ -13,19 +11,19 @@ const resultTable = document.getElementsByTagName('tbody')[0];
 const displayRange = document.getElementById('displayRange');
 
 
+var listResults = JSON.parse(localStorage.getItem('listResults')) || [];
+
+renderTable();
 
 inputPowerEletric.addEventListener('input', function () {
     displayRange.textContent = this.value;
 });
 
-var countTable = 1;
 
 axios.get('./public/data.json').then(
     function (response) {
-        // console.log(response.data);
         data = response.data;
         data.forEach(element => {
-            // console.log(element)
             var locationItem = document.createElement('option');
             locationItem.setAttribute('value', element.ideTarifaFornecimento);
             var locationText = document.createTextNode(element.sigDistribuidora);
@@ -36,40 +34,60 @@ axios.get('./public/data.json').then(
 ).catch(
     function (error) {
         console.warn(error);
-        alert("Ocorreu um erro inesperdo! Recarregue e página e tente novamente.")
+        alert('Ocorreu um erro inesperdo! Recarregue e página e tente novamente.');
     }
 );
-
 
 function calculate() {
 
     var powerEletric = inputPowerEletric.value;
-    var amountDevices =inputAmountDevices.value;
+    var amountDevices = inputAmountDevices.value;
     var useDuration = inputUseDuration.value;
     var period = inputPeriod.value;
     var deviceName = inputDeviceName.value;
     var id = cbPowerSupply.value;
-
+    var powerSupply;
     var tariff = null;
+
+
 
     data.forEach(
         element => {
             if (element.ideTarifaFornecimento == id) {
                 tariff = element.vlrTotaTRFConvencional;
+                powerSupply = element.sigDistribuidora;
             }
         }
     );
 
     if (tariff !== null) {
-        var kw = (amountDevices * powerEletric * useDuration * period) / 1000;
-        console.log(kw + " kw");
 
-        cost = kw * tariff;
+        //parses
+        powerEletricScale = document.getElementById('cbPowerEletricScale').value;
+        switch (powerEletricScale) {
+            case 'Watts':
+                powerEletric /= 1000;
+                break;
+            case 'Mega Watts':
+                powerEletric *= 1000;
+                break;
+        }
 
-        var kw = (amountDevices * powerEletric * useDuration * period) / 1000;
-        console.log(kw + " kw");
+        var useDurationNotParsed = useDuration;
+        timeUseType = document.getElementById('cbTimeUseType').value;
+        switch (timeUseType) {
+            case 'Minutos':
+                useDuration /= 60;
+                break;
+            case 'Segundos':
+                useDuration /= 3600;
+                break;
+        }
 
-        cost = kw * tariff;
+        var electricalConsumption = (amountDevices * powerEletric * useDuration * period);
+        console.log(electricalConsumption + " kw");
+
+        cost = electricalConsumption * tariff;
 
         var costFormatted = cost.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
@@ -79,44 +97,100 @@ function calculate() {
         resultText.textContent = costFormatted
         console.log(cost);
 
-        // style="display: none"
-        // resultTable.style.display = 'block';
-        var resultRow = document.createElement('tr');
+        deviceName = deviceName || '-';
 
+        addResult(
+            {
+                'deviceName': deviceName,
+                'amountDevices': amountDevices,
+                'useDuration': useDurationNotParsed + ' ' + timeUseType,
+                'electricalConsumption': electricalConsumption,
+                'period': period,
+                'powerSupply': powerSupply,
+                'cost': costFormatted
+            }
+        );
+
+    }
+}
+
+
+function addResult(result) {
+    console.log(result);
+    listResults.push(result);
+    renderTable();
+    saveToStorage();
+}
+
+function deleteResult(index) {
+    listResults.splice((index), 1);
+    renderTable();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('listResults', JSON.stringify(listResults));
+}
+
+
+function renderTable() {
+    resultTable.innerHTML = '';
+    var index = 0;
+    for (result of listResults) {
         var tableCount = document.createElement('th');
-        var tableCountText = document.createTextNode(countTable);
+        var tableCountText = document.createTextNode((index + 1));
         tableCount.appendChild(tableCountText);
 
-
+        var resultRow = document.createElement('tr');
         var resultName = document.createElement('td');
-        var resultNameText;
-
-        if (deviceName !== '') {
-            resultNameText = document.createTextNode(deviceName);
-        } else {
-            resultNameText = document.createTextNode("-");
-        }
+        var resultNameText = document.createTextNode(result.deviceName);
 
         resultName.appendChild(resultNameText);
+        var resultAmountDevices = document.createElement('td');
+        var resultAmountDevicesText = document.createTextNode(result.amountDevices);
+        resultAmountDevices.appendChild(resultAmountDevicesText);
+
+        var resultUseDuration = document.createElement('td');
+        var resultUseDurationText = document.createTextNode(result.useDuration);
+        resultUseDuration.appendChild(resultUseDurationText);
 
         var resultElectricalConsumption = document.createElement('td');
-        var resultElectricalConsumptionText = document.createTextNode(kw + " kw");
+        var resultElectricalConsumptionText = document.createTextNode(result.electricalConsumption + ' KW');
         resultElectricalConsumption.appendChild(resultElectricalConsumptionText);
 
-        var resultCost = document.createElement('td');
-        resultCost.appendChild(document.createTextNode(costFormatted));
+        var resultUsePeriod = document.createElement('td');
+        var reusltUsePeriodText = document.createTextNode(result.period + ' dias');
+        resultUsePeriod.appendChild(reusltUsePeriodText);
 
+        var resultPowerSupply = document.createElement('td');
+        var resultPowerSupplyText = document.createTextNode(result.powerSupply);
+        resultPowerSupply.appendChild(resultPowerSupplyText);
+
+        var resultCost = document.createElement('td');
+        resultCost.appendChild(document.createTextNode(result.cost));
+
+        var removeIconButton = document.createElement('td');
+
+        var removeButton = document.createElement('a');
+        removeButton.setAttribute('onclick', 'deleteResult(' + index + ')');
+
+        var removeIcon = document.createElement('span');
+        removeIcon.setAttribute('class', 'material-icons removeButton');
+        removeIcon.textContent = 'delete';
+
+        removeButton.appendChild(removeIcon);
+        removeIconButton.appendChild(removeButton);
 
         resultRow.appendChild(tableCount);
         resultRow.appendChild(resultName);
+        resultRow.appendChild(resultAmountDevices);
+        resultRow.appendChild(resultUseDuration);
         resultRow.appendChild(resultElectricalConsumption);
+        resultRow.appendChild(resultUsePeriod);
+        resultRow.appendChild(resultPowerSupply);
         resultRow.appendChild(resultCost);
-
+        resultRow.appendChild(removeIconButton);
         resultTable.appendChild(resultRow);
-
-        countTable++;
+        index++;
     }
-
-
-
 }
